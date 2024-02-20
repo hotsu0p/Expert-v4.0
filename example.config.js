c302fe850e431c0dcd0513019765c03dc8f99ebb
Mona Lisa <jwstephenson1000@gmail.com>
2024-02-19 20:59:13 -0600

    added example

---
+++ example.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    port: 8000, // Pport to listen on
+    prefix: "!", // Prefix for commands
+    id: "", // Discord Bot ID
+    usingCustomDomain: false, // Use a custom domain for the bot
+    domain: "http://localhost/", // Custom domain
+    discordInvite: "https://discord.gg/FWJsSnVGgM", // Discord Invite URL
+    mongodbUrl: "", // MongoDB connection URL
+    clientSecret: "", // Client Secret from Discord Application
+    token: "", // Bot Token from Discord Application
+  };
