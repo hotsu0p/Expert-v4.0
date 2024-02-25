3b1edd41addc2648b3def816173d7dcc814764c7
Mona Lisa <jwstephenson1000@gmail.com>
2024-02-24 18:17:52 -0600

    j

--- example.config.js
+++ example.config.js
@@ -1,12 +1,11 @@
-
 module.exports = {
-  port: 8000, // Pport to listen on
-  prefix: "!", // Prefix for commands
-  id: "1168691870809989293", // Discord Bot ID
-  usingCustomDomain: false, // Use a custom domain for the bot
-  domain: "http://localhost", // Custom domain
-  discordInvite: "https://discord.gg/FWJsSnVGgM", // Discord Invite URL
-  mongodbUrl: "mongodb+srv://hotsuop:quWh6KdCq1vnyCTu@darling.w6irx0u.mongodb.net/?retryWrites=true&w=majority&appName=darling", // MongoDB connection URL
-  clientSecret: "EIsQ0p6IkbqX3N-crYIeiioTd9GMTaeS", // Client Secret from Discord Application
-  token: "MTE2ODY5MTg3MDgwOTk4OTI5Mw.Gi3Ty8._8RsL3VejMxi7SdbpQXNK6djXI5PCKmtyC1TFM", // Bot Token from Discord Application
-};
+    port: 8000, // Pport to listen on
+    prefix: "!", // Prefix for commands
+    id: "", // Discord Bot ID
+    usingCustomDomain: false, // Use a custom domain for the bot
+    domain: "http://localhost/", // Custom domain
+    discordInvite: "https://discord.gg/FWJsSnVGgM", // Discord Invite URL
+    mongodbUrl: "", // MongoDB connection URL
+    clientSecret: "", // Client Secret from Discord Application
+    token: "", // Bot Token from Discord Application
+  };
